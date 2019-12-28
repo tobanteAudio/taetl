5df170dc4add42c49095fee9d1687269778b164b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-28 06:24:04 +0000

    Deploy tobanteAudio/taetl to github.com/tobanteAudio/taetl.git:gh-pages

--- search/all_10.js
+++ search/all_10.js
@@ -4,6 +4,7 @@ var searchData=
   ['taetl',['TAETL',['../index.html',1,'']]],
   ['taetl',['taetl',['../namespacetaetl.html',1,'']]],
   ['taetl_5fis_5fclass',['TAETL_IS_CLASS',['../intrinsics_8hpp.html#a5e5322628b3857868ee48f4baa81d7e0',1,'intrinsics.hpp']]],
+  ['taetl_5fis_5fenum',['TAETL_IS_ENUM',['../intrinsics_8hpp.html#ab9bcd9311214c4c96d23ecd719499f25',1,'intrinsics.hpp']]],
   ['taetl_5fis_5funion',['TAETL_IS_UNION',['../intrinsics_8hpp.html#ae0d1f9cf9b90fa012b8ec4ec5c7c6fb3',1,'intrinsics.hpp']]],
   ['taetl_5frevision_5fmajor',['TAETL_REVISION_MAJOR',['../version_8hpp.html#a943b16722664a179422a12ebfa874abc',1,'version.hpp']]],
   ['taetl_5frevision_5fminor',['TAETL_REVISION_MINOR',['../version_8hpp.html#a42addfb36e83f7f5101e0983ebcca9ad',1,'version.hpp']]],
--- search/variables_1.js
+++ search/variables_1.js
@@ -2,10 +2,12 @@ var searchData=
 [
   ['is_5farray_5fv',['is_array_v',['../namespacetaetl.html#a065a45c9cb208bfff1d04f2d7f22486a',1,'taetl']]],
   ['is_5fclass_5fv',['is_class_v',['../namespacetaetl.html#a7e576e3366734ef0897ee7914f063929',1,'taetl']]],
+  ['is_5fenum_5fv',['is_enum_v',['../namespacetaetl.html#a1d239347c64adf7d1652a0f797207f17',1,'taetl']]],
   ['is_5ffloating_5fpoint_5fv',['is_floating_point_v',['../namespacetaetl.html#a8c66850e5bd17940311cdb146708ec9b',1,'taetl']]],
   ['is_5fintegral_5fv',['is_integral_v',['../namespacetaetl.html#a4b7b396559f603c1aada741158138e06',1,'taetl']]],
   ['is_5fnull_5fpointer_5fv',['is_null_pointer_v',['../namespacetaetl.html#ae979684c0c41cbe4b58b83b364accd63',1,'taetl']]],
   ['is_5fpointer_5fv',['is_pointer_v',['../namespacetaetl.html#a207e3cfba69a7220ffd4e88f8cf6189b',1,'taetl']]],
   ['is_5fsame_5fv',['is_same_v',['../namespacetaetl.html#a9bf9a3bde398663de7399abe8454822c',1,'taetl']]],
+  ['is_5funion_5fv',['is_union_v',['../namespacetaetl.html#a9520ee3ca2de999b948a680644d39832',1,'taetl']]],
   ['is_5fvoid_5fv',['is_void_v',['../namespacetaetl.html#a907685ffa9cfcd92c62717924722dddf',1,'taetl']]]
 ];
