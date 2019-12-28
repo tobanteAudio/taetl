f9a35ae0bfa4eebbc123e7d3f76a8d5853d7be5c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-28 06:40:29 +0000

    Deploy tobanteAudio/taetl to github.com/tobanteAudio/taetl.git:gh-pages

--- search/functions_c.js
+++ search/functions_c.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['size',['size',['../classtaetl_1_1Array.html#abdb280fec527919a28f959be965aaca9',1,'taetl::Array::size()'],['../classtaetl_1_1String.html#a4e35e03703a9de5be33323ca0a82a250',1,'taetl::String::size()']]],
-  ['string',['String',['../classtaetl_1_1String.html#a82a65a09a226fc3f3da6414f1d01ac21',1,'taetl::String::String()=default'],['../classtaetl_1_1String.html#a6bc206d151aacdcafa91547d4882366f',1,'taetl::String::String(const char *c_string) noexcept']]],
+  ['string',['String',['../classtaetl_1_1String.html#a82a65a09a226fc3f3da6414f1d01ac21',1,'taetl::String::String()=default'],['../classtaetl_1_1String.html#a55fa6fd71c982c5249d90e3577c5f2e6',1,'taetl::String::String(const char *c_string, taetl::size_t length) noexcept'],['../classtaetl_1_1String.html#a6bc206d151aacdcafa91547d4882366f',1,'taetl::String::String(const char *c_string) noexcept']]],
   ['strlen',['strlen',['../namespacetaetl.html#af268ab31c7b18ec7dff24ab944677fcb',1,'taetl']]]
 ];
