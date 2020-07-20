3d273a663b19bed1b9c322f9dfc15173c47c0eac
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-20 21:21:08 +0000

    Deploy tobanteAudio/taetl to github.com/tobanteAudio/taetl.git:gh-pages

--- search/functions_13.js
+++ search/functions_13.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['val',['val',['../namespaceetl_1_1hardware_1_1stm32.html#ace4beda7c5c027a4b833c9d51589b27e',1,'etl::hardware::stm32']]],
   ['value',['value',['../structetl_1_1experimental_1_1units_1_1quantity.html#ab04aedb5a314fed72fc35cfa8fb31729',1,'etl::experimental::units::quantity::value()'],['../structetl_1_1GetImpl.html#a1cbce0d847a6acf90f08263dba7f33b0',1,'etl::GetImpl::value()'],['../structetl_1_1GetImpl_3_010_00_01First_00_01Rest_8_8_8_01_4.html#a24bfd136a7c64d70d1d96de0e3bf35b0',1,'etl::GetImpl&lt; 0, First, Rest... &gt;::value()']]],
-  ['vector',['vector',['../classetl_1_1vector.html#a6e332e2973edc768d5d71071969fd505',1,'etl::vector::vector()'],['../classetl_1_1make_1_1vector.html#a874e612c2889156aae32ec57b8e6b12b',1,'etl::make::vector::vector()']]],
+  ['vector',['vector',['../classetl_1_1vector.html#ae5551119940dc6ba2becdb47b6a542ed',1,'etl::vector::vector()'],['../classetl_1_1make_1_1vector.html#a874e612c2889156aae32ec57b8e6b12b',1,'etl::make::vector::vector()']]],
   ['vportyield',['vPortYield',['../stubs_8hpp.html#a7c16eb2af816c607da9badb56e9b8d42',1,'stubs.hpp']]],
   ['vqueuedelete',['vQueueDelete',['../stubs_8hpp.html#a153422cc5143e3f5715349d0092146bb',1,'stubs.hpp']]],
   ['vtaskdelay',['vTaskDelay',['../stubs_8hpp.html#aab45dfd5e56c051cd4cef90b16848b30',1,'stubs.hpp']]],
