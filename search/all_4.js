bc7f7db1d473542b6377a74428584cc4183bc350
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-04 06:46:08 +0000

    Deploy tobanteAudio/taetl to github.com/tobanteAudio/taetl.git:gh-pages

--- search/all_4.js
+++ search/all_4.js
@@ -31,10 +31,11 @@ var searchData=
   ['executable_5fformat_5ferror',['executable_format_error',['../namespaceetl.html#abf855e66ccd0c031e0994ed578f1c170a4dfaf9c996cf045f32d17edc9754fc95',1,'etl']]],
   ['experimental',['experimental',['../namespaceetl_1_1experimental.html',1,'etl']]],
   ['hardware',['hardware',['../namespaceetl_1_1hardware.html',1,'etl']]],
-  ['literals',['literals',['../namespaceetl_1_1dsp_1_1literals.html',1,'etl::dsp']]],
+  ['literals',['literals',['../namespaceetl_1_1dsp_1_1literals.html',1,'etl::dsp::literals'],['../namespaceetl_1_1literals.html',1,'etl::literals']]],
   ['make',['make',['../namespaceetl_1_1make.html',1,'etl']]],
   ['mcp23017',['mcp23017',['../namespaceetl_1_1hardware_1_1mcp23017.html',1,'etl::hardware']]],
   ['net',['net',['../namespaceetl_1_1net.html',1,'etl']]],
   ['rtos',['rtos',['../namespaceetl_1_1rtos.html',1,'etl']]],
-  ['stm32',['stm32',['../namespaceetl_1_1hardware_1_1stm32.html',1,'etl::hardware']]]
+  ['stm32',['stm32',['../namespaceetl_1_1hardware_1_1stm32.html',1,'etl::hardware']]],
+  ['string_5fview_5fliterals',['string_view_literals',['../namespaceetl_1_1literals_1_1string__view__literals.html',1,'etl::literals']]]
 ];
