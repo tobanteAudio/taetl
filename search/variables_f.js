6b86cc73075c37ecfc4e5c507cc346fdbe85a163
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-08 23:56:33 +0000

    Deploy tobanteAudio/taetl to github.com/tobanteAudio/taetl.git:gh-pages

--- search/variables_f.js
+++ search/variables_f.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['value',['value',['../structetl_1_1integral__constant.html#ac6a0d38654d5eeea44e25fea4eae7a55',1,'etl::integral_constant']]]
+  ['value',['value',['../structetl_1_1integral__constant.html#ac6a0d38654d5eeea44e25fea4eae7a55',1,'etl::integral_constant::value()'],['../classetl_1_1is__trivially__copyable.html#a9d6a1f9c6afffff012c6fff48e6cd5ee',1,'etl::is_trivially_copyable::value()']]]
 ];
