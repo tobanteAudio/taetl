34a2fa1338599199a8a14eb062bf6a83152d0627
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-10 17:39:48 +0000

    Deploy tobanteAudio/taetl to github.com/tobanteAudio/taetl.git:gh-pages

--- search/all_6.js
+++ search/all_6.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['gcd',['gcd',['../namespaceetl.html#ab65cd6f8748c172eab6ee36130dca3c3',1,'etl']]],
+  ['generate',['generate',['../namespaceetl.html#a0b963791bdb1da038a64b23555f245c4',1,'etl']]],
+  ['generate_5fn',['generate_n',['../namespaceetl.html#ab984bfa2eb0fd6da7369b629ca3acdc7',1,'etl']]],
   ['get',['get',['../classetl_1_1small__ptr.html#a72b5b4add52fd479b5fdfb54fbd2864f',1,'etl::small_ptr::get() noexcept -&gt; Type *'],['../classetl_1_1small__ptr.html#a260cc982f6e7feb2f6c7bca1fa5604a4',1,'etl::small_ptr::get() const noexcept -&gt; Type const *'],['../namespaceetl.html#a9df6d2b5d1046470baf1626470258ed2',1,'etl::get()']]],
   ['giga',['giga',['../namespaceetl.html#aeaa010c22727dfd8ede60afef156650c',1,'etl']]],
   ['gpintena',['GPINTENA',['../namespaceetl_1_1hardware_1_1mcp23017.html#aa623c967f42a82f6bf4c33a99fdda77ca03993b5654f15a8140aa212a0a0166d5',1,'etl::hardware::mcp23017']]],
--- search/functions_6.js
+++ search/functions_6.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['gcd',['gcd',['../namespaceetl.html#ab65cd6f8748c172eab6ee36130dca3c3',1,'etl']]],
+  ['generate',['generate',['../namespaceetl.html#a0b963791bdb1da038a64b23555f245c4',1,'etl']]],
+  ['generate_5fn',['generate_n',['../namespaceetl.html#ab984bfa2eb0fd6da7369b629ca3acdc7',1,'etl']]],
   ['get',['get',['../classetl_1_1small__ptr.html#a72b5b4add52fd479b5fdfb54fbd2864f',1,'etl::small_ptr::get() noexcept -&gt; Type *'],['../classetl_1_1small__ptr.html#a260cc982f6e7feb2f6c7bca1fa5604a4',1,'etl::small_ptr::get() const noexcept -&gt; Type const *'],['../namespaceetl.html#a9df6d2b5d1046470baf1626470258ed2',1,'etl::get()']]]
 ];
