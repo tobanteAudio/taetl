e7f320139cfb2f509b4022bae39e3ff135ab762f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-29 16:01:19 +0000

    Deploy tobanteAudio/taetl to github.com/tobanteAudio/taetl.git:gh-pages

--- search/all_b.js
+++ search/all_b.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['language_5fstandard',['language_standard',['../namespaceetl.html#a3d70da9977ec456339973b30e372fcbd',1,'etl']]],
   ['lcm',['lcm',['../namespaceetl.html#a88d17ac0d457a45babf1c999ed6bea7f',1,'etl']]],
-  ['length',['length',['../structetl_1_1char__traits_3_01char_01_4.html#a2ec57591fba911fe5f55c5fc416dab59',1,'etl::char_traits&lt; char &gt;::length()'],['../classetl_1_1basic__string.html#afc92ea5165b480b959c446ccc77239cf',1,'etl::basic_string::length()'],['../classetl_1_1basic__string__view.html#a43bc851d32e3a0ebb9ec4b8be2c95de5',1,'etl::basic_string_view::length()']]],
+  ['length',['length',['../structetl_1_1char__traits_3_01char_01_4.html#a2ec57591fba911fe5f55c5fc416dab59',1,'etl::char_traits&lt; char &gt;::length()'],['../classetl_1_1basic__static__string.html#afe480b26da6fad988f1730bef354c357',1,'etl::basic_static_string::length()'],['../classetl_1_1basic__string__view.html#a43bc851d32e3a0ebb9ec4b8be2c95de5',1,'etl::basic_string_view::length()']]],
   ['less',['less',['../structetl_1_1less.html',1,'etl']]],
   ['less_3c_20void_20_3e',['less&lt; void &gt;',['../structetl_1_1less_3_01void_01_4.html',1,'etl']]],
   ['less_5fequal',['less_equal',['../structetl_1_1less__equal.html',1,'etl']]],
--- search/classes_1.js
+++ search/classes_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['back_5finsert_5fiterator',['back_insert_iterator',['../classetl_1_1back__insert__iterator.html',1,'etl']]],
-  ['basic_5fstring',['basic_string',['../classetl_1_1basic__string.html',1,'etl']]],
+  ['basic_5fstatic_5fstring',['basic_static_string',['../classetl_1_1basic__static__string.html',1,'etl']]],
   ['basic_5fstring_5fview',['basic_string_view',['../classetl_1_1basic__string__view.html',1,'etl']]],
   ['bidirectional_5fiterator_5ftag',['bidirectional_iterator_tag',['../structetl_1_1bidirectional__iterator__tag.html',1,'etl']]],
   ['bit_5fand',['bit_and',['../structetl_1_1bit__and.html',1,'etl']]],
