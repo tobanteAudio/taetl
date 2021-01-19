cd5c69a63db9df8f6b5afaecccbd7fff5015f045
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-19 19:47:06 +0000

    Deploy tobanteAudio/taetl to github.com/tobanteAudio/taetl.git:gh-pages

--- search/functions_12.js
+++ search/functions_12.js
@@ -8,7 +8,7 @@ var searchData=
   ['tolower',['tolower',['../namespaceetl.html#a25b969e80e81b5a412d56884fc5bfcf2',1,'etl']]],
   ['top',['top',['../classetl_1_1stack.html#a91252dabd80f39762b5f4fc39e8d83b9',1,'etl::stack::top() -&gt; reference'],['../classetl_1_1stack.html#a8dc0af3afb7dff71e9e6cfa8947eb88c',1,'etl::stack::top() const -&gt; const_reference']]],
   ['toupper',['toupper',['../namespaceetl.html#a2b46f5faca236c0de0b9f378c22498c1',1,'etl']]],
-  ['transform',['transform',['../namespaceetl.html#a32dd2ffddadb248b341e42879a9e9951',1,'etl::transform(InputIter first, InputIter last, OutputIter destination, UnaryOperation op) -&gt; OutputIter'],['../namespaceetl.html#a85f81702ed6fdeecec082a39e50e4e2a',1,'etl::transform(InputIter1 first1, InputIter1 last1, InputIter2 first2, OutputIter dest, BinaryOperation op) -&gt; OutputIter']]],
+  ['transform',['transform',['../namespaceetl.html#a704b8d21ad6879a3a28be5c0bd435fa4',1,'etl::transform(InputIter first, InputIter last, OutputIter dest, UnaryOperation op) -&gt; OutputIter'],['../namespaceetl.html#a85f81702ed6fdeecec082a39e50e4e2a',1,'etl::transform(InputIter1 first1, InputIter1 last1, InputIter2 first2, OutputIter dest, BinaryOperation op) -&gt; OutputIter']]],
   ['try_5fto_5flock_5ft',['try_to_lock_t',['../structetl_1_1try__to__lock__t.html#af06a493d8a0eaf3bfb6dae59654e30a5',1,'etl::try_to_lock_t']]],
   ['tuple',['tuple',['../structetl_1_1tuple.html#ab8e76ac29d0cbefcacf8542472cb57e4',1,'etl::tuple::tuple()'],['../structetl_1_1tuple_3_01First_01_4.html#aa04ae60dbe7c1cd3fc614143e58759e2',1,'etl::tuple&lt; First &gt;::tuple()']]]
 ];
